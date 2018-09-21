0a6b96d1000335d70e2313895ca8e009db880b79
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-20 22:55:48 -0400

    adding structure

--- interviewgame/src/App.test.js
+++ interviewgame/src/App.test.js
@@ -1653,3 +1653,185 @@ function log(string) {
   const lineNumber = callerLine.match(/:(\d+):\d+\)/)[1];
   console.log("Line " + lineNumber + ": " + string);
 }
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
+/ Function Expression
+const multiply1 = function(a, b) {
+  return a * b;
+};
+
+// Arrow Function Expression
+const multiply2 = (a, b) => {
+  return a * b;
+};
+
+multiply2(2, 3); // 6 - called the same way as usual
+
+// Arrow Function Expression - concise
+const multiply3 = (a, b) => a * b;
+// without curly brackets, the return statement is implied
+
+// can omit the parenthesis if there's only a single parameter
+const square = x => x * x;
+
+
+// Note: there's no arrow-function equivalent of function declarations
+function multiply4(a, b) {
+  return a * b;
+}
+
+// ¯\_(ツ)_/¯
